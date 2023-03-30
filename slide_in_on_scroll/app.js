efdfc88c69edb1c235c41002f0d08074d53f770e
lazyhysong <37135523+eee0930@users.noreply.github.com>
2023-03-30 14:06:55 +0900

    feat: slide in on scroll

---
+++ slide_in_on_scroll/app.js
@@ -0,0 +1,38 @@
+// 스크롤 이벤트가 너무 많이 불리는 것을 방지 (매번 default 20ms 기다리고 count)
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+  // console.log(e);
+  // console.count(e);
+  // console.log(window.scrollY);
+  sliderImages.forEach(sliderImage => {
+    //  half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if(isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
